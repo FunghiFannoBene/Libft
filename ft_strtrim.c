773c4f778a050b8462396a1732869f5bd94689a8
FunghiFannoBene <119907315+FunghiFannoBene@users.noreply.github.com>
2023-07-06 21:16:35 +0200

    Update ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,56 +12,41 @@
 
 #include "libft.h"
 
-size_t	find(const char *s1, const char *set, size_t start)
+static int
+	ft_char_in_set(char c, char const *set)
 {
 	size_t	i;
 
 	i = 0;
-	while (set[i] != '\0' && *s1)
+	while (set[i])
 	{
-		if (!(set[i] == s1[start]))
-			i++;
-		else if (set[i] == s1[start++])
-			i = 0;
-	}
-	return (start);
-}
-
-size_t	findend(const char *s1, const char *set, size_t end)
-{
-	size_t	i;
-
-	i = 0;
-	while (set[i] != '\0' && *s1)
-	{
-		if (!(set[i] == s1[end]))
-			i++;
-		else if (set[i] == s1[end--])
-			i = 0;
-	}
-	return (end);
-}
-
-char	*ft_strtrim(char const *s1, char const *set)
-{
-	char	*result;
-	size_t	start;
-	size_t	end;
-	size_t	i;
-
-	start = 0;
-	end = ft_strlen(s1) - 1;
-	start = find(s1, set, start);
-	end = findend(s1, set, end);
-	result = (char *)malloc(sizeof(*s1) * (end - start + 2));
-	i = 0;
-	while (start <= end)
-	{
-		result[i] = s1[start++];
+		if (set[i] == c)
+			return (1);
 		i++;
 	}
-	if (!result)
+	return (0);
+}
+
+char
+	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*str;
+	size_t	i;
+	size_t	start;
+	size_t	end;
+
+	start = 0;
+	while (s1[start] && ft_char_in_set(s1[start], set))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && ft_char_in_set(s1[end - 1], set))
+		end--;
+	str = (char*)malloc(sizeof(*s1) * (end - start + 1));
+	if (!str)
 		return (NULL);
-	result[i] = '\0';
-	return (result);
+	i = 0;
+	while (start < end)
+		str[i++] = s1[start++];
+	str[i] = 0;
+	return (str);
 }
